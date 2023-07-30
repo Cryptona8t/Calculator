536360587892d54570bdd44ba316df0a8985a9c9
Nicholas <114204580+Cryptona8t@users.noreply.github.com>
2023-07-30 13:59:09 +0200

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -1,11 +1,14 @@
 def factorial(n):
+    
     if n == 0 or n == 1:
         return 1
     else:
         return n * factorial(n - 1)
+
 def main():
     try:
         num = int(input("Enter a number: "))
+        
         if num < 0:
             print("Factorial is not defined for negative numbers.")
         else:
@@ -13,5 +16,6 @@ def main():
             print(f"The factorial of {num} is: {result}")
     except ValueError:
         print("Invalid input. Please enter a valid integer.")
+
 if __name__ == "__main__":
     main()
